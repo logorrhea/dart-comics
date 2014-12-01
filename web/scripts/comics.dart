d2f2272b24b0763716b09ab226743b5d7a46c5b5
Tyler Funk <tfunk@fastmail.fm>
2014-11-30 21:28:50 -0600

    Finished first section of front-end code

--- web/scripts/comics.dart
+++ web/scripts/comics.dart
@@ -2,5 +2,34 @@ import 'dart:html';
 import 'dart:convert';
 
 main() {
-    // Do stuff here
+    
+    // Query for comic list
+    var list_el = document.query('#comics-list');
+
+    // Create Ajax request object
+    var req = new HttpRequest();
+
+    // Configure and send the request
+    req.open('get', '/comics');
+    req.onLoad.listen((req) {
+        var list = JSON.decode(req.target.responseText);
+        list_el.innerHtml = graphic_novels_template(list);
+    });
+    req.send();
+}
+
+graphic_novels_template(list) {
+    var html = '';
+    list.forEach((graphic_novel) {
+        html += graphic_novel_template(graphic_novel);
+    });
+    return html;
+}
+
+graphic_novel_template(graphic_novel) {
+    return '''
+        <li id="${graphic_novel['id']}">
+            ${graphic_novel['title']}
+            <a class="delete" href="#">[delete]</a>
+        </li>''';
 }
